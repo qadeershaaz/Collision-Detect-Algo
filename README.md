58b3d15951a529bb54515f7a8f82298b57956fd5
qadeershaaz <77328007+qadeershaaz@users.noreply.github.com>
2021-02-01 18:57:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Collision-Detect-Algo
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,36 @@
+var fixedrect;
+var movingrect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedrect=createSprite(500,100,50,50);
+  fixedrect.debug=true;
+  movingrect=createSprite(300,200,50,50);
+  movingrect.debug=true;
+  movingrect.shapeColor="green";
+  fixedrect.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255); 
+  movingrect.x=World.mouseX;
+ movingrect.y=World.mouseY;
+
+if(movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2 
+  && fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2
+  && fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2
+  && movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2
+  )
+{
+  movingrect.shapeColor="red";
+  fixedrect.shapeColor="red";
+}
+  else{movingrect.shapeColor="green";
+  fixedrect.shapeColor="green";
+  }
+
+
+
+  drawSprites();
+}
